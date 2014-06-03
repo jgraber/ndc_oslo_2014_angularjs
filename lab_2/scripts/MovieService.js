2f486abd108761e71532436122d23eb24d582dcb
Johnny Graber <jg@jgraber.ch>
2014-06-03 08:57:30 +0200

    2. Lab

---
+++ lab_2/scripts/MovieService.js
@@ -0,0 +1,15 @@
+﻿(function () {
+
+    var movieService = function ($http) {
+
+        return {
+            getAll: function () {
+                return $http.get("http://localhost:3000/api/movies");
+            }
+        };
+    };
+
+    var module = angular.module("atTheMovies");
+    module.factory("movieService", movieService);
+
+}());

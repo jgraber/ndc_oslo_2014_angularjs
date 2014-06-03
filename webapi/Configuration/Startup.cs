4ae45b4286d6e2ac72c4834e91d3f3b6ce8b7ebf
Johnny Graber <jg@jgraber.ch>
2014-06-03 08:56:42 +0200

    add webapi webservice from OdeToCode

---
+++ webapi/Configuration/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Http;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Newtonsoft.Json.Serialization;
+using Owin;
+
+namespace AtTheMovies.Configuration
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //UseStaticFiles(app);
+            UseWebApi(app);
+        }
+
+        private static void UseWebApi(IAppBuilder app)
+        {
+            var apiConfiguration = new HttpConfiguration();
+            apiConfiguration.MapHttpAttributeRoutes();
+            apiConfiguration.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver();
+
+			app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(apiConfiguration);
+        }
+
+        private void UseStaticFiles(IAppBuilder app)
+        {
+            app.UseFileServer(new FileServerOptions
+            {
+                FileSystem = new PhysicalFileSystem(@"..\..\..\..\clients"),
+                EnableDefaultFiles = true
+            });
+        }
+    }
+}

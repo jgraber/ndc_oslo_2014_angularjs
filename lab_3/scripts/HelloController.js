138be010acc194e5771f350f5324b3abc75cb04a
Johnny Graber <jg@jgraber.ch>
2014-06-03 08:57:44 +0200

    3. Lab

---
+++ lab_3/scripts/HelloController.js
@@ -0,0 +1,17 @@
+﻿(function () {
+
+    var HelloController = function ($scope) {
+
+        $scope.showMessage = function () {
+            $scope.message = "Hello, World!";
+        };
+
+        $scope.saveMessage = function () {
+            $scope.alert = "Saved...";
+        };
+
+    };
+
+    var module = angular.module("atTheMovies");
+    module.controller("HelloController", HelloController);
+}());

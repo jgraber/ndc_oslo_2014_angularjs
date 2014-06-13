518c73bac9c9bbed3c381e23c8c916a54363153f
Johnny Graber <jg@jgraber.ch>
2014-06-13 23:50:16 +0200

    redo lab 1

---
+++ lab_1/scripts/MoviesController.js
@@ -0,0 +1,18 @@
+﻿var MoviesController = function ($scope) {
+
+    var movies = [
+        { title: "Star Wars", length: 120, released: 1981 },
+        { title: "Top Gun", length: 90, released: 1984 },
+        { title: "Hot Shots", length: 89, released: 1986 }
+    ];
+
+    $scope.movies = movies;
+
+    $scope.increase = function (movie) {
+            movie.length += 1;
+        };
+
+    $scope.decrease = function (movie) {
+        movie.length -= 1;
+    };
+};

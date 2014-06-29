a7f40e96c9ad7494a6a8d379a15cad2002f4fe46
Johnny Graber <jg@jgraber.ch>
2014-06-29 22:22:14 +0200

    start lab 6 with copy of lab 5

---
+++ lab_6/scripts/MovieService.js
@@ -0,0 +1,18 @@
+﻿(function () {
+
+    var movieService = function ($http) {
+
+        return {
+            getAll: function () {
+                return $http.get("http://localhost:8080/api/movies");
+            },
+            save: function (movie) {
+                return $http.put("http://localhost:8080/api/movies", movie);
+            }
+        };
+    };
+
+    var module = angular.module("atTheMovies");
+    module.factory("movieService", movieService);
+
+}());

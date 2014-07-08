8f8ebdd43d3686fb14fadf58ef579763f0b387dc
Johnny Graber <jg@jgraber.ch>
2014-07-08 19:43:58 +0200

    start lab 8 with copy of lab 7

---
+++ lab_8/scripts/MovieService.js
@@ -0,0 +1,48 @@
+﻿(function () {
+
+    var module = angular.module("atTheMovies.data", []);
+    module.config(function ($provide) {
+        $provide.provider("movieService", function () {
+
+            var url;
+
+            this.setRootUrl = function (newUrl) {
+                url = newUrl;
+            };
+
+            this.$get = function ($http, $q, $timeout) {
+
+                var movies = null;
+
+                return {
+
+                    getById: function(id) {
+                        return $http.get(url + "/" + id)
+                                    .then(function(response) {
+                                        return response.data;
+                                    });
+                    },
+
+                    getAll: function () {
+
+                        if (movies) {
+                            return $q.when(movies);
+                        } else {
+
+                            return $http.get(url)
+                                .then(function (response) {
+                                    movies = response.data;
+                                    return movies;
+                                });
+                        }
+                    },
+                    save: function (movie) {
+                        movies = null;
+                        return $http.put(url, movie);
+                    }
+                };
+            };
+
+        });
+    });
+}());
---
+++ lab_8/scripts/atTheMovies.js
@@ -0,0 +1,37 @@
+﻿(function () {
+
+var module = angular.module("atTheMovies",
+        ["ngRoute", "atTheMovies.data"]);
+
+    module.config(function ($httpProvider) {
+
+    });
+
+    module.config(function (movieServiceProvider) {
+        movieServiceProvider.setRootUrl("http://localhost:8080/api/movies");
+    });
+
+    module.config(function($routeProvider) {
+        $routeProvider
+            .when("/list", {
+                templateUrl: "views/list.html",
+                controller: "MovieListController"                
+            })
+            .when("/edit/:id", {                
+                templateUrl: "views/edit.html",
+                controller: "MovieEditController"
+            })
+            .when("/:id", {                
+                templateUrl: "views/detail.html",
+                controller: "MovieEditController"
+            })
+            .otherwise({                
+                redirectTo: "/list" 
+            });
+    });
+
+    module.run(function ($rootScope) {
+        $rootScope.version = "v1.0";
+    });
+
+}());
